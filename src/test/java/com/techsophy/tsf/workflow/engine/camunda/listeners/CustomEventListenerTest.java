245318751b4e4d87a87c259bf7ab5ecf5c1c5d69
Jagadish <jagadish.b@techsophy.com>
2022-10-03 12:10:42 +0530

    Test cases

--- src/test/java/com/techsophy/tsf/workflow/engine/camunda/listeners/CustomEventListenerTest.java
+++ src/test/java/com/techsophy/tsf/workflow/engine/camunda/listeners/CustomEventListenerTest.java
@@ -5,6 +5,12 @@ import com.techsophy.tsf.workflow.engine.camunda.model.ChecklistInstanceResponse
 import com.techsophy.tsf.workflow.engine.camunda.service.ChecklistService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.model.bpmn.Query;
+import org.camunda.bpm.model.bpmn.instance.ExtensionElements;
+import org.camunda.bpm.model.bpmn.instance.UserTask;
+import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperties;
+import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperty;
+import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,14 +21,13 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.techsophy.tsf.workflow.engine.camunda.constants.CheckListConstants.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomEventListenerTest {
@@ -34,6 +39,8 @@ public class CustomEventListenerTest {
     ChecklistService checklistService;
     @Mock
     DelegateExecution delegateExecution;
+    @Mock
+    CamundaProperty camundaProperty;
     @InjectMocks
     CustomEventListener customEventListener;
 
@@ -60,6 +67,8 @@ public class CustomEventListenerTest {
         Mockito.when(delegateTask.getVariable(CHECKLIST_INSTANCE_ID)).thenReturn("abc");
         Mockito.when(checklistService.getChecklistInstanceById(delegateTask.getVariable(CHECKLIST_INSTANCE_ID).toString())).thenReturn(checklistInstanceResponseModel);
         customEventListener.onTaskCompleteEvent(delegateTask);
+        Mockito.verify(delegateTask,times(2)).getVariable(CHECKLIST_INSTANCE_ID);
+
     }
 
     @Test
@@ -83,15 +92,46 @@ public class CustomEventListenerTest {
         Assertions.assertThrows(IllegalArgumentException.class,()->customEventListener.onTaskCompleteEvent(delegateTask));
     }
 
-//    @Test
-//    void onTaskCreatedEventNullTest(){
-//        UserTask userTask = mock(UserTask.class);
-//        Mockito.when(delegateTask.getBpmnModelElementInstance()).thenReturn(userTask);
-//        customEventListener.onTaskCreatedEvent(delegateTask);
-//    }
+    @Test
+    void onTaskCreatedEventNullTest(){
+        UserTask userTask = mock(UserTask.class);
+        Mockito.when(delegateTask.getBpmnModelElementInstance()).thenReturn(userTask);
+        customEventListener.onTaskCreatedEvent(delegateTask);
+        Mockito.verify(delegateTask,times(1)).getBpmnModelElementInstance();
+    }
 
-//    @Test
-//    void onTaskCreatedEventTest(){
-//
-//    }
+    @Test
+    void onTaskCreatedEventZeroCountTest(){
+        UserTask userTask = mock(UserTask.class);
+        Query<ModelElementInstance> query = mock(Query.class);
+        Query<CamundaProperties> propertiesQuery = mock(Query.class);
+        ExtensionElements extensionElements = mock(ExtensionElements.class);
+        Mockito.when(delegateTask.getBpmnModelElementInstance()).thenReturn(userTask);
+        Mockito.when(userTask.getExtensionElements()).thenReturn(extensionElements);
+        Mockito.when(extensionElements.getElementsQuery()).thenReturn(query);
+        Mockito.when(query.filterByType(CamundaProperties.class)).thenReturn(propertiesQuery);
+        customEventListener.onTaskCreatedEvent(delegateTask);
+        Mockito.verify(delegateTask,times(1)).getBpmnModelElementInstance();
+    }
+
+    @Test
+    void onTaskCreatedEventWithCountValueTest(){
+        CamundaProperties camundaProperties = mock(CamundaProperties.class);
+        Collection<CamundaProperty> cproperty=mock(Collection.class);
+        cproperty.add(camundaProperty);
+        System.out.println("text "+camundaProperty.getCamundaName());
+        UserTask userTask = mock(UserTask.class);
+        Mockito.when(delegateTask.getBpmnModelElementInstance()).thenReturn(userTask);
+        ExtensionElements extensionElements = mock(ExtensionElements.class);
+        Mockito.when(userTask.getExtensionElements()).thenReturn(extensionElements);
+        Query<CamundaProperties> query = mock(Query.class);
+        Query<ModelElementInstance> modelElement = mock(Query.class);
+        Mockito.when(extensionElements.getElementsQuery()).thenReturn(modelElement);
+        Mockito.when(modelElement.filterByType(CamundaProperties.class)).thenReturn(query);
+        Mockito.when(query.count()).thenReturn(1);
+        Mockito.when(query.singleResult()).thenReturn(camundaProperties);
+        Mockito.when(camundaProperties.getCamundaProperties()).thenReturn(cproperty);
+        customEventListener.onTaskCreatedEvent(delegateTask);
+        Mockito.verify(delegateTask,times(1)).getBpmnModelElementInstance();
+    }
 }
