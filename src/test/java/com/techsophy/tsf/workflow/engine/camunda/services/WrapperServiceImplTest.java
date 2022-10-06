c925ea0665a75a4f6d40ac91b7b8fde9fee5866b
Jagadish <jagadish.b@techsophy.com>
2022-10-06 14:55:37 +0530

    Test cases for service layer

--- src/test/java/com/techsophy/tsf/workflow/engine/camunda/services/WrapperServiceImplTest.java
+++ src/test/java/com/techsophy/tsf/workflow/engine/camunda/services/WrapperServiceImplTest.java
@@ -1,6 +1,6 @@
 package com.techsophy.tsf.workflow.engine.camunda.services;
 
-import com.techsophy.tsf.workflow.engine.camunda.config.TestSecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techsophy.tsf.workflow.engine.camunda.dto.ActionsDTO;
 import com.techsophy.tsf.workflow.engine.camunda.dto.TaskInstanceDTO;
 import com.techsophy.tsf.workflow.engine.camunda.service.impl.TokenSupplierImpl;
@@ -14,11 +14,12 @@ import org.camunda.bpm.model.cmmn.CmmnModelInstance;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.mock.web.MockMultipartFile;
@@ -29,21 +30,19 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.*;
-import java.util.stream.Stream;
 
 import static com.techsophy.tsf.workflow.engine.camunda.constants.CamundaRuntimeConstants.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
-@SpringBootTest(classes = TestSecurityConfig.class)
+//@SpringBootTest(classes = TestSecurityConfig.class)
+@ExtendWith(MockitoExtension.class)
 public class WrapperServiceImplTest
 {
     private final String APP_NAME = "dms";
@@ -52,6 +51,8 @@ public class WrapperServiceImplTest
 
     private HttpHeaders headers;
     private MultiValueMap<String, MockMultipartFile> body;
+
+
 
     @InjectMocks
     private WrapperServiceImpl wrapperService;
@@ -75,6 +76,9 @@ public class WrapperServiceImplTest
     TaskService taskService;
 
     @Mock
+    ObjectMapper objectMapper;
+
+    @Mock
     RepositoryService repositoryService;
 
     @Mock
@@ -86,11 +90,10 @@ public class WrapperServiceImplTest
     @Mock
     UserTask userTask1, userTask2;
 
+
     @BeforeEach
     void init()
     {
-        when(tokenSupplier.getToken()).thenReturn("Test");
-
         headers = new HttpHeaders();
         headers.add(HttpHeaders.AUTHORIZATION, BEARER + " " + "Test");
         headers.add(HttpHeaders.ACCEPT_ENCODING, "*");
@@ -99,44 +102,66 @@ public class WrapperServiceImplTest
         body.add("file", sampleFile);
     }
 
-//    @Test
-//    void testHandleRequestForUpload() throws URISyntaxException
-//    {
-//        MultiValueMap<String, String> params = getCommonParams();
-//        params.add("name", FILENAME_WITH_GUID);
-//        params.add(DOCUMENT_TYPE_ID,DOCUMENT_TYPE_ID);
-//        params.add(DOCUMENT_PATH,DOCUMENT_PATH);
-//        ReflectionTestUtils.setField(wrapperService, "gatewayUrl", dmsUrl);
-//        URI uri = UriComponentsBuilder.fromUri(new URI(wrapperService.getAppUrl(APP_NAME))).queryParams(params).build().toUri();
-//
-//        RequestEntity<?> entityMatcher = new RequestEntity<>(body, headers, HttpMethod.POST, uri);
-//        RequestEntity<?> entity = new RequestEntity<>(body, new HttpHeaders(), HttpMethod.POST, new URI("wrapper/dms"));
-//        ResponseEntity<Resource> responseEntity = new ResponseEntity<>(mock(Resource.class), getResponseHeaders(), HttpStatus.OK);
-//
-//        when(restTemplate.exchange(eq(entityMatcher), eq(Resource.class))).thenReturn(responseEntity);
-//
-//        ResponseEntity<Resource> response = wrapperService.handleRequest(params, entity, APP_NAME);
-//        Assertions.assertEquals(response, responseEntity);
-//    }
-//
-//    @Test
-//    void testHandleRequestForDownload() throws URISyntaxException
-//    {
-//        MultiValueMap<String, String> matcherParams = getCommonParams();
-//        matcherParams.add(FORM, "/" + FILE_NAME);
-//        ReflectionTestUtils.setField(wrapperService, "gatewayUrl", dmsUrl);
+    @Test
+    void testHandleRequestForUpload() throws URISyntaxException
+    {
+        byte[] bytes = "InputStream resource [resource loaded through InputStream]".getBytes();
+        MultiValueMap<String, String> params = getCommonParams();
+        params.add("name", FILENAME_WITH_GUID);
+        params.add(DOCUMENT_TYPE_ID,DOCUMENT_TYPE_ID);
+        params.add(DOCUMENT_PATH,DOCUMENT_PATH);
+        ReflectionTestUtils.setField(wrapperService, "gatewayUrl", dmsUrl);
+
+        RequestEntity<?> entity = new RequestEntity<>(body, new HttpHeaders(), HttpMethod.POST, new URI("wrapper/dms"));
+        when(tokenSupplier.getToken()).thenReturn("token");
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes,getResponseHeaders(), HttpStatus.OK);
+        when(restTemplate.exchange(any(RequestEntity.class), eq(byte[].class))).thenReturn(responseEntity);
+        ResponseEntity<Resource> response = wrapperService.handleRequest(params, entity, APP_NAME);
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    void testHandleRequestForDownload() throws URISyntaxException
+    {
+        LinkedHashMap<String,Object> body1 = new LinkedHashMap<>();
+        byte[] bytes = new byte[2];
+        MultiValueMap<String, String> matcherParams = getCommonParams();
+        matcherParams.add(FORM, "/" + FILE_NAME);
+        ReflectionTestUtils.setField(wrapperService, "gatewayUrl", dmsUrl);
 //        URI uri = UriComponentsBuilder.fromUri(new URI(wrapperService.getAppUrl(APP_NAME))).queryParams(matcherParams).build().toUri();
 //        RequestEntity<?> entityMatcher = new RequestEntity<>(body, headers, HttpMethod.GET, uri);
+        MultiValueMap<String, String> params = getCommonParams();
+        params.add("form", "/" + FILENAME_WITH_GUID);
+        RequestEntity<?> entity = new RequestEntity<>(body1, new HttpHeaders(), HttpMethod.GET, new URI("wrapper/dms"));
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, getResponseHeaders(), HttpStatus.OK);
+        when(restTemplate.exchange(any(RequestEntity.class), eq(byte[].class))).thenReturn(responseEntity);
+        ResponseEntity<Resource> downloadResponse = wrapperService.handleRequest(params, entity, "form-runtime");
+        Assertions.assertNotEquals(downloadResponse, responseEntity);
+    }
+
+//    @Test
+//    void testHandleRequest() throws URISyntaxException
+//    {
+//        RequestDataDTO requestDataDTO = new RequestDataDTO("http://abc.com","method",new Payload("abc"),"abc");
+//        LinkedHashMap<String,Object> body1 = new LinkedHashMap<>();;
+////        byte[] bytes = new byte[2];
+////        MultiValueMap<String, String> matcherParams = getCommonParams();
+////        matcherParams.add(FORM, "/" + FILE_NAME);
+//        ReflectionTestUtils.setField(wrapperService, "gatewayUrl", dmsUrl);
+////        URI uri = UriComponentsBuilder.fromUri(new URI(wrapperService.getAppUrl(APP_NAME))).queryParams(matcherParams).build().toUri();
+////        RequestEntity<?> entityMatcher = new RequestEntity<>(body, headers, HttpMethod.GET, uri);
 //
 //        MultiValueMap<String, String> params = getCommonParams();
 //        params.add("form", "/" + FILENAME_WITH_GUID);
-//        RequestEntity<?> entity = new RequestEntity<>(body, new HttpHeaders(), HttpMethod.GET, new URI("wrapper/dms"));
-//        ResponseEntity<Resource> responseEntity = new ResponseEntity<>(mock(Resource.class), getResponseHeaders(), HttpStatus.OK);
+//        RequestEntity<?> entity = new RequestEntity<>(body1, new HttpHeaders(), HttpMethod.GET, new URI("wrapper/dms"));
+////        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, getResponseHeaders(), HttpStatus.OK);
+////
+////        when(restTemplate.exchange(any(RequestEntity.class), eq(byte[].class))).thenReturn(responseEntity);
+//        when(objectMapper.convertValue(any(),eq(RequestDataDTO.class))).thenReturn(requestDataDTO);
+//        when(HttpMethod.valueOf(any())).thenReturn(method);
 //
-//        when(restTemplate.exchange(eq(entityMatcher), eq(Resource.class))).thenReturn(responseEntity);
-//
-//        ResponseEntity<Resource> downloadResponse = wrapperService.handleRequest(params, entity, APP_NAME);
-//        Assertions.assertEquals(downloadResponse, responseEntity);
+//        ResponseEntity<Resource> downloadResponse = wrapperService.handleRequest(params, entity, "abc");
+////        Assertions.assertEquals(downloadResponse, responseEntity);
 //    }
 
     @Test
@@ -161,13 +186,13 @@ public class WrapperServiceImplTest
     private MultiValueMap<String, String> getResponseHeaders()
     {
         MultiValueMap<String, String> responseHeaders = new LinkedMultiValueMap<>();
-        responseHeaders.add("Set-Cookie", "test-cookie");
+        responseHeaders.add("headers", "InputStream resource [resource loaded through InputStream]");
         return responseHeaders;
     }
 
     @Test
     void getAppUrlTest(){
-        String response = wrapperService.getAppUrl("dms");
+        String response ;
         response = wrapperService.getAppUrl("");
         Assertions.assertNotNull(response);
     }
