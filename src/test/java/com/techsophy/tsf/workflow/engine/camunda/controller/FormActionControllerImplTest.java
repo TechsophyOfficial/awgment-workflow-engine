ba39ab43acf7aafc25ee0bdbee9d24a462da0f89
Jagadish <jagadish.b@techsophy.com>
2022-10-07 18:03:49 +0530

    Test cases for Controller layer

---
+++ src/test/java/com/techsophy/tsf/workflow/engine/camunda/controller/FormActionControllerImplTest.java
@@ -0,0 +1,154 @@
+package com.techsophy.tsf.workflow.engine.camunda.controller;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.techsophy.tsf.workflow.engine.camunda.controller.runtime.impl.FormActionControllerImpl;
+import com.techsophy.tsf.workflow.engine.camunda.dto.*;
+import com.techsophy.tsf.workflow.engine.camunda.exception.GlobalExceptionHandler;
+import com.techsophy.tsf.workflow.engine.camunda.model.ApiResponse;
+import com.techsophy.tsf.workflow.engine.camunda.service.impl.RuntimeProcessServiceImpl;
+import org.camunda.bpm.engine.task.Task;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.*;
+
+import static com.techsophy.tsf.workflow.engine.camunda.constants.CamundaRuntimeConstants.*;
+import static com.techsophy.tsf.workflow.engine.camunda.constants.FormConstants.*;
+import static org.camunda.bpm.engine.rest.impl.DeploymentRestServiceImpl.TENANT_ID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ExtendWith(MockitoExtension.class)
+public class FormActionControllerImplTest {
+
+    private static  final SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor jwtRead = jwt().authorities(new SimpleGrantedAuthority("READ"));
+
+    @Mock
+    RuntimeProcessServiceImpl runtimeProcessService;
+
+    @Mock
+    ObjectMapper objectMapper;
+
+    @Mock
+    TaskQueryDTO taskQueryDTO;
+
+    @Autowired
+    FormSchemaResponse response;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    FormActionControllerImpl formActionController;
+
+
+    @BeforeEach
+    public void setUp()
+    {
+        mockMvc = MockMvcBuilders.standaloneSetup(formActionController)
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
+    }
+
+    @Test
+    void getAllTasksTest() throws Exception {
+        ActionsDTO actionsDTO = new ActionsDTO("label","http://abc.com","action","abc");
+        TaskInstanceDTO taskInstanceDTO = new TaskInstanceDTO(ID,NAME,ASSIGNEEE,new Date(),new Date(),new Date(),DELEGATION_STATE,DESCRIPTION,"1",
+                "owner",PARENT_TASK_ID,1,"processDefinitionId",PROCESS_INSTANCE_ID,"taskdefinitionKey",
+                "caseExecutionId","caseInstanceId","caseDefinitionId",false,FORM_KEY,TENANT_ID,BUSINESS_KEY,
+                "componentId","type","abc",CHECKLIST_INSTANCE_ID_FOR_PROCESS_INSTANCE,List.of(actionsDTO));
+        ObjectMapper objectMapperTest=new ObjectMapper();
+        List<TaskInstanceDTO> list = new ArrayList<>();
+        list.add(taskInstanceDTO);
+        PaginationDTO<List<TaskInstanceDTO>> paginationDTO= new PaginationDTO<>(list,1,1,1, 1L,1L);
+        Mockito.when(runtimeProcessService.getAllTasks(any(),any(),any())).thenReturn(paginationDTO);
+        RequestBuilder requestBuilderTest = MockMvcRequestBuilders.get(BASE_URL + TASK_URL)
+                .with(jwtRead).content(objectMapperTest.writeValueAsString(taskInstanceDTO)).contentType(MediaType.APPLICATION_JSON);
+        MvcResult mvcResult = this.mockMvc.perform(requestBuilderTest).andExpect(status().isOk()).andReturn();
+        assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    void getNextTaskTest() {
+        Mockito.when(runtimeProcessService.getNextTask(anyString())).thenReturn(response);
+        ApiResponse<FormSchemaResponse> expected = new ApiResponse<>(response,true,GET_FORM_SUCCESS);
+        ApiResponse<FormSchemaResponse> actual = formActionController.getNextTask(BUSINESS_KEY);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void getSubTasksTest() throws Exception {
+        Task task = mock(Task.class);
+        Mockito.when(runtimeProcessService.getSubTasks(any())).thenReturn(List.of(task));
+        ApiResponse<List<Task>> expected = new ApiResponse<>(List.of(task),true,"sub tasks fetched  successfully");
+        ApiResponse<List<Task>> actual = formActionController.getSubTasks(TASK_ID);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void createProcessInstanceTest(){
+        Map<String,Object> variable = new HashMap<>();
+        variable.put("key","value");
+        ProcessInstanceDTO processInstanceDTO = new ProcessInstanceDTO("processDefinitionKey",variable,BUSINESS_KEY,FORM_KEY);
+        ProcessInstanceResponseDTO processInstanceResponseDTO = new ProcessInstanceResponseDTO(PROCESS_INSTANCE_ID,BUSINESS_KEY,variable,true);
+        Mockito.when(runtimeProcessService.createProcessInstance(any())).thenReturn(processInstanceResponseDTO);
+        ApiResponse<ProcessInstanceResponseDTO> expected = new ApiResponse<>(processInstanceResponseDTO,true,PROCESS_CREATION);
+        ApiResponse<ProcessInstanceResponseDTO> actual = formActionController.createProcessInstance(processInstanceDTO);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void getAllTasksByQueryTest(){
+        ActionsDTO actionsDTO = new ActionsDTO("label","http://abc.com","action","abc");
+        TaskInstanceDTO taskInstanceDTO = new TaskInstanceDTO(ID,NAME,ASSIGNEEE,new Date(),new Date(),new Date(),DELEGATION_STATE,DESCRIPTION,"1",
+                "owner",PARENT_TASK_ID,1,"processDefinitionId",PROCESS_INSTANCE_ID,"taskdefinitionKey",
+                "caseExecutionId","caseInstanceId","caseDefinitionId",false,FORM_KEY,TENANT_ID,BUSINESS_KEY,
+                "componentId","type","abc",CHECKLIST_INSTANCE_ID_FOR_PROCESS_INSTANCE,List.of(actionsDTO));
+        PaginationDTO<List<TaskInstanceDTO>> expected = new PaginationDTO<>(List.of(taskInstanceDTO),1,1,1,1L,1L);
+        Mockito.when(runtimeProcessService.getAllTasks(any(),any(),any())).thenReturn(expected);
+        PaginationDTO<List<TaskInstanceDTO>> actual = formActionController.getAllTasksByQuery(taskQueryDTO,1,1);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void getAllHistoryTasksByQueryTest(){
+        HistoricInstanceDTO historicInstanceDTO = mock(HistoricInstanceDTO.class);
+        HistoricQueryInstanceDTO historicQueryInstanceDTO = mock(HistoricQueryInstanceDTO.class);
+        PaginationDTO<List<HistoricInstanceDTO>> expected = new PaginationDTO<>(List.of(historicInstanceDTO),1,1,1,1L,1L);
+        Mockito.when(runtimeProcessService.getAllHistoryTask(any(),any(),any())).thenReturn(expected);
+        PaginationDTO<List<HistoricInstanceDTO>> actual = formActionController.getAllHistoryTasksByQuery(historicQueryInstanceDTO,1,1);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void completeAndFetchNextTaskTest(){
+        Map<String,Object> variable = new HashMap<>();
+        variable.put("key","value");
+        TaskDTO taskDTO = new TaskDTO(BUSINESS_KEY,TASK_ID,variable);
+        Mockito.when(runtimeProcessService.completeAndFetchNextTask(any())).thenReturn(response);
+        ApiResponse<FormSchemaResponse> expected = new ApiResponse<>(response,true,NO_FORM_AVAILABLE);
+        ApiResponse<FormSchemaResponse> actual = formActionController.completeAndFetchNextTask(taskDTO);
+        Assertions.assertEquals(expected,actual);
+    }
+}
--- src/test/java/com/techsophy/tsf/workflow/engine/camunda/controller/FormControllerTest.java
+++ src/test/java/com/techsophy/tsf/workflow/engine/camunda/controller/FormControllerTest.java
@@ -1,91 +1,91 @@
 package com.techsophy.tsf.workflow.engine.camunda.controller;
 
-import au.com.origin.snapshots.junit5.SnapshotExtension;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
-import com.techsophy.tsf.workflow.engine.camunda.config.TestAuthorizationExtension;
-import com.techsophy.tsf.workflow.engine.camunda.config.TestKeycloakServerExtension;
-import com.techsophy.tsf.workflow.engine.camunda.config.TestSecurityConfig;
+import com.techsophy.tsf.workflow.engine.camunda.config.GlobalMessageSource;
+import com.techsophy.tsf.workflow.engine.camunda.controller.runtime.impl.FormControllerImpl;
 import com.techsophy.tsf.workflow.engine.camunda.dto.FormSchema;
-import com.techsophy.tsf.workflow.engine.camunda.service.impl.WrapperServiceImpl;
-import lombok.Cleanup;
-import lombok.RequiredArgsConstructor;
+import com.techsophy.tsf.workflow.engine.camunda.exception.GlobalExceptionHandler;
+import com.techsophy.tsf.workflow.engine.camunda.service.RuntimeFormService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
-import static au.com.origin.snapshots.SnapshotMatcher.expect;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.techsophy.tsf.workflow.engine.camunda.constants.CamundaRuntimeConstants.BASE_URL;
+
+import static com.techsophy.tsf.workflow.engine.camunda.constants.CamundaRuntimeConstants.*;
+import static com.techsophy.tsf.workflow.engine.camunda.constants.FormConstants.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = TestSecurityConfig.class)
-@ActiveProfiles("test")
-@AutoConfigureMockMvc
-@ExtendWith(SnapshotExtension.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@RequiredArgsConstructor(onConstructor_ = {@Autowired})
-@ExtendWith({TestKeycloakServerExtension.class, TestAuthorizationExtension.class})
+@ExtendWith(MockitoExtension.class)
 class FormControllerTest
 {
-    private static final String FORM_CONTENT_DATA = "testdata/form-content.json";
+    private static  final SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor jwtRead = jwt().authorities(new SimpleGrantedAuthority("READ"));
 
-    private final MockMvc mockMvc;
-    private final ObjectMapper mapper;
-    private final WrapperServiceImpl wrapperService;
-    private static final JwtRequestPostProcessor USER1_JWT = jwt().jwt(jwt -> jwt.claim("preferred_username", "user1@test.com"));
+    @Mock
+    RuntimeFormService runtimeFormService;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    @Mock
+    GlobalMessageSource globalMessageSource;
+    @InjectMocks
+    FormControllerImpl formController;
+
+    private static final String FORM_CONTENT_DATA = "testdata/form-content.json";
     private static final WireMockServer GATEWAY_URL=new WireMockServer(6666);
 
-    @BeforeAll void init() throws IOException {
-        GATEWAY_URL.start();
-        @Cleanup InputStream contactDetails = new ClassPathResource(FORM_CONTENT_DATA).getInputStream();
-        FormSchema form = this.mapper.readValue(contactDetails, FormSchema.class);
-        GATEWAY_URL.stubFor(get("/form-runtime/v1/forms/FormKeyTest")
-                .willReturn(json(mapper.writeValueAsString(Map.of(
-                        "data",form,
-                        "success",true,
-                        "message","Form details detched successfully")))));
+    @BeforeEach public void setUp()
+    {
+        mockMvc = MockMvcBuilders.standaloneSetup(formController)
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
     }
 
     public static ResponseDefinitionBuilder json(String apiResponse)
     {
         return aResponse().withHeader("Content-Type",MediaType.APPLICATION_JSON_VALUE).withBody(apiResponse);
     }
-
-    @AfterAll
-    void destroy(){
-        GATEWAY_URL.resetAll();
-        GATEWAY_URL.stop();
+    @Test
+    @Order(1)
+    void getFormByKeyTest() throws Exception
+    {
+        InputStream inputStreamTest=new ClassPathResource(FORM_CONTENT).getInputStream();
+        ObjectMapper objectMapperTest=new ObjectMapper();
+        FormSchema formSchemaTest=objectMapperTest.readValue(inputStreamTest,FormSchema.class);
+        Map<String,Object> component = new HashMap<>();
+        component.put("key","value");
+        FormSchema formSchema =  new FormSchema(NAME,TEST_ID,component,TEST_VERSION);
+        Mockito.when(runtimeFormService.fetchFormById(any(),any())).thenReturn(formSchema);
+        RequestBuilder requestbuilder =
+                MockMvcRequestBuilders.get(BASE_URL + "/forms/{formKey}",1)
+                        .content(objectMapperTest.writeValueAsString(formSchemaTest)).content(objectMapperTest.writeValueAsString(formSchema))
+                        .with(jwtRead)
+                        .contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestbuilder).andExpect(status().isOk()).andReturn();
+        assertEquals(200,result.getResponse().getStatus());
     }
-
-//    @Test
-//    @Order(1)
-//    void getFormByKeyTest() throws Exception
-//    {
-//        RequestBuilder requestbuilder =
-//                MockMvcRequestBuilders.get(BASE_URL + "/forms/FormKeyTest?task=null")
-//                      .with(USER1_JWT).contentType(MediaType.APPLICATION_JSON);
-//        MvcResult result = mockMvc.perform(requestbuilder).andExpect(status().isOk()).andReturn();
-//        expect(this.mapper.readValue(result.getResponse().getContentAsByteArray(), Object.class)).orderedJson().toMatchSnapshot();
-//    }
-
-
 }
